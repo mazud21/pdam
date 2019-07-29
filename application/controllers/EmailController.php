041ff3f0d824625488bc7b6f402dc2b406b968dc
masud <hmazud@gmail.com>
2019-07-29 11:02:20 +0700

    tried send email(2)

--- application/controllers/EmailController.php
+++ application/controllers/EmailController.php
@@ -6,40 +6,45 @@ class EmailController extends CI_Controller {
 
     public function __construct() {
         parent:: __construct();
-
-        $this->load->helper('url');
     }
 
     public function index()
     {
+        $this->load->view('contact');
 
-        $config = [
-               'mailtype'  => 'html',
-               'charset'   => 'utf-8',
-               'protocol'  => 'smtp',
-               'smtp_host' => 'ssl://smtp.gmail.com',
-               'smtp_user' => 'user@gmail.com',    
-               'smtp_pass' => 'user_pass',      
-               'smtp_port' => '465',
-               'crlf'      => "\r\n",
-               'newline'   => "\r\n"
-           ];
-
-        $this->load->library('email', $config);
-
-        $this->email->from('no-reply@pdam.com', 'PDAM Kabupaten Bantul');
-
-        $this->email->to('user@gmail.com'); 
-
-        $this->email->subject('VALIDASI AKUN PDAM MOBILE');
-
-        $this->email->message("No Pelanggan :  <br> Password : ");
-
-        if ($this->email->send()) {
-            echo 'Sukses! email berhasil dikirim.';
-            redirect('EmailController');
-        } else {
-            echo 'Error! email tidak dapat dikirim.';
-        }
+        if ($this->input->post('sendEmail')) {
+            $config = [
+                'mailtype'  => 'html',
+                'charset'   => 'utf-8',
+                'protocol'  => 'smtp',
+                'smtp_host' => 'ssl://smtp.gmail.com',
+                'smtp_user' => 'user@gmail.com',    
+                'smtp_pass' => 'user_pass',      
+                'smtp_port' => '465',
+                'crlf'      => "\r\n",
+                'newline'   => "\r\n"
+            ];
+    
+            $to_email = $this->input->post('email');   
+            $message = $this->input->post('message');   
+        
+            $this->load->library('email', $config);
+        
+            $this->email->from('no-reply@pdam.com', 'PDAM Kabupaten Bantul');
+        
+            $this->email->to($to_email); 
+        
+            $this->email->subject('VALIDASI AKUN PDAM MOBILE');
+        
+            $this->email->message($message);
+        
+            if ($this->email->send()) {
+                echo 'Sukses! email dapat dikirim.';
+                
+            } else {
+                echo 'Error! email tidak dapat dikirim.';
+            }
+        }     
     }
+
 }
