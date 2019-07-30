699e7a9849acfec95c2dee8b1fe88d8a7f458a39
masud <hmazud@gmail.com>
2019-07-30 23:57:31 +0700

    add data tarif

--- assets/pdam_rest.sql
+++ assets/pdam_rest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2019 at 05:12 PM
+-- Generation Time: Jul 30, 2019 at 06:55 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -44,12 +44,7 @@ CREATE TABLE `masalah_air` (
 --
 
 INSERT INTO `masalah_air` (`no_info`, `wilayah`, `hari`, `tanggal`, `estimasi`, `kerusakan`, `alternatif`, `penanganan`) VALUES
-(2, 'aaaaaaa', 'dddd', '2019-06-22', '01:00:00', 'cccccc', 'bbbbbb', 'qqqqqqqqq'),
-(3, 'eeeee', 'dddd', '2019-06-22', '01:00:00', 'cccccc', 'bbbbbb', 'gggaaaagggg'),
-(4, 'eeeee', 'dddd', '2019-06-22', '01:00:00', 'cccccc', 'bbbbbb', 'gggaaaagggg'),
-(5, 'yyyyyyy', 'bbbbb', '2019-06-22', '01:00:00', 'eeeeeee', 'bbbbbb', 'aaaaaaa'),
-(6, 'eeeee', 'dddd', '2019-06-22', '01:00:00', 'cccccc', 'bbbbbb', 'ggggggg'),
-(7, 'eeeee', 'dddd', '2019-06-22', '01:00:00', 'cccccc', 'bbbbbb', 'ggggggg');
+(9, 'aaaaeeeeeaaa', 'dddd', '2019-07-02', '01:00:00', 'cccccc', 'ffffffff', 'gggaaaagggg');
 
 -- --------------------------------------------------------
 
@@ -64,7 +59,7 @@ CREATE TABLE `pelanggan` (
   `alamat` text NOT NULL,
   `email` varchar(25) NOT NULL,
   `no_hp` varchar(12) NOT NULL,
-  `foto_ktp` varchar(25) NOT NULL,
+  `foto_ktp` varchar(100) NOT NULL,
   `pilih_tarif` char(4) NOT NULL,
   `no_pelanggan` int(6) DEFAULT NULL,
   `password` varchar(25) DEFAULT NULL
@@ -75,9 +70,12 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`no_daftar`, `no_ktp`, `nama`, `alamat`, `email`, `no_hp`, `foto_ktp`, `pilih_tarif`, `no_pelanggan`, `password`) VALUES
-(3, 2147483647, 'hanabi', 'kirigakure', 'hanzo@gmail.com', '12345678901', 'djfnskjdf.jpg', 'A3', 4, '23456789'),
-(4, 2147483647, 'dfsgdhrt', 'sfddhdsg', 'hijrahadv22@gmail.com', '234567456789', 'dfsgh.jpg', 'A1', 8, 'fdggfdg4356'),
-(5, 9876542340987089, 'hanabi n', 'hhhhh', 'hm@gmail.com', '234567456789', 'djfnskjdf.png', 'A1', 90, 'fdggfdg4356');
+(1, 2147483647098765, 'upload', 'sfddhdsg', 'hijrahadv22@gmail.com', '234567456789', 'hac.jpg', 'A1', 1, 'fdggfdg4356'),
+(2, 9876542340987089, 'upload', 'sfddhdsg', 'upload@gmail.com', '234567456789', 'Kyoukai-no-Kanata-Wallpaper-Kanbara-Akihito.jpg', 'A1', 2, 'upload'),
+(3, 2147483647098765, 'hanabi n', 'sfddhdsg', 'upload@gmail.com', '99999999999', '1543cafdff8fc6e10463c711c388c02c94b4d221_hq.jpg', 'B1', 3, 'fdggfdg4356'),
+(4, 2147483647098765, 'aaaaaaaaa', 'sfddhdsg', 'hm@gmail.com', '34567456789', 'samurai_chamlpoo.png', 'A1', 4, 'fdggfdg4356'),
+(5, 2147483647098765, 'upload', 'sfddhdsg', 'renzo@gmail.com', '123456723421', 'ao-haru-ride-kou-futaba-under-the-stars.jpg', 'A3', 6, '85741620'),
+(6, 9876542340987089, 'hanabi n', 'sfddhdsg', 'hijrahadv22@gmail.com', '234567456789', '1543cafdff8fc6e10463c711c388c02c94b4d221_hq.jpg', 'B3', 5, '98164705');
 
 -- --------------------------------------------------------
 
@@ -131,7 +129,38 @@ INSERT INTO `tagihan_air` (`no_tagihan`, `no_daftar`, `denda`, `bulan_bayar`, `b
 (6, 4, 1, '0000-00-00', 40001, 1, 2400, 2500, '2019-07-09', 1, 1, 40000, 'sudah'),
 (7, 0, 0, '0000-00-00', 50000, 0, 4000, 4500, NULL, 0, 0, 50000, ''),
 (8, 0, 0, '0000-00-00', 50000, 0, 4000, 4500, NULL, 0, 0, 50000, ''),
-(9, 5, 3333, '0000-00-00', 555556, 9999, 4009, 4507, '0000-00-00', 5555, 4444, 555553, 'sudah');
+(9, 5, 3333, '0000-00-00', 555556, 9999, 4009, 4507, '0000-00-00', 5555, 4444, 555553, 'sudah'),
+(10, 12, 0, '0000-00-00', 40001, 0, 4000, 4500, NULL, 0, 0, 40001, 'belum');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tarif_air`
+--
+
+CREATE TABLE `tarif_air` (
+  `id_tarif_air` char(5) NOT NULL,
+  `tarif` int(5) NOT NULL,
+  `ket_tarif` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tarif_air`
+--
+
+INSERT INTO `tarif_air` (`id_tarif_air`, `tarif`, `ket_tarif`) VALUES
+('A1_1', 2750, 'Rumah Tipe A1(0-10 m3)'),
+('A1_2', 3900, 'Rumah Tipe A1(11-20 m3)'),
+('A1_3', 4400, 'Rumah Tipe A1(>20 m3)'),
+('A2_1', 4500, 'Rumah Tipe A2(0-10 m3)'),
+('A2_2', 4800, 'Rumah Tipe A2(11-20 m3)'),
+('A2_3', 5400, 'Rumah Tipe A2(>20 m3)'),
+('SK_1', 2500, 'Sosial Khusus(0-10 m3)'),
+('SK_2', 3400, 'Sosial Khusus(11-20 m3)'),
+('SK_3', 4100, 'Sosial Khusus(>20 m3)'),
+('SU_1', 2200, 'Sosial Umum(0-10 m3)'),
+('SU_2', 2800, 'Sosial Umum(11-20 m3)'),
+('SU_3', 3200, 'Sosial Umum(>20 m3)');
 
 --
 -- Indexes for dumped tables
@@ -162,6 +191,12 @@ ALTER TABLE `tagihan_air`
   ADD PRIMARY KEY (`no_tagihan`);
 
 --
+-- Indexes for table `tarif_air`
+--
+ALTER TABLE `tarif_air`
+  ADD PRIMARY KEY (`id_tarif_air`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -169,13 +204,13 @@ ALTER TABLE `tagihan_air`
 -- AUTO_INCREMENT for table `masalah_air`
 --
 ALTER TABLE `masalah_air`
-  MODIFY `no_info` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `no_info` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `no_daftar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `no_daftar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `pengaduan`
@@ -187,7 +222,7 @@ ALTER TABLE `pengaduan`
 -- AUTO_INCREMENT for table `tagihan_air`
 --
 ALTER TABLE `tagihan_air`
-  MODIFY `no_tagihan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `no_tagihan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
