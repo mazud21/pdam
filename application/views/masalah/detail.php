fe793c870c47d85f5da555ce7104c77763d01ccf
masud <hmazud@gmail.com>
2019-07-31 20:30:33 +0700

    add label

--- application/views/masalah/detail.php
+++ application/views/masalah/detail.php
@@ -7,16 +7,38 @@
                     Detail Data Masalah Air
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title"><?= $masalah_air['no_info']; ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted">
-                    <?= $masalah_air['wilayah']; ?></h6>
-                    <p class="card-text"><?= $masalah_air['hari']; ?></p>
-                    <p class="card-text"><?= date('d M Y', strtotime($masalah_air['tanggal']))?></p>
-                    <p class="card-text"><?= $masalah_air['estimasi']; ?></p>
-                    <p class="card-text"><?= $masalah_air['kerusakan']; ?></p>
-                    <p class="card-text"><?= $masalah_air['alternatif']; ?></p>
-                    <p class="card-text"><?= $masalah_air['penanganan']; ?></p>
-                    
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Nomor Masalah</h6></label><br>
+                            <?= $masalah_air['no_info']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Wilayah</h6></label><br>
+                            <?= $masalah_air['wilayah']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Hari</h6></label><br>
+                            <?= $masalah_air['hari']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Tanggal</h6></label><br>
+                            <?= date('d M Y', strtotime($masalah_air['tanggal'])) ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Estimasi Perbaikan</h6></label><br>
+                            <?= $masalah_air['estimasi']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Kerusakan</h6></label><br>
+                            <?= $masalah_air['kerusakan']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Alternatif</h6></label><br>
+                            <?= $masalah_air['alternatif']; ?>
+                        </div>
+                    <div class="form-group">
+                            <label for=""><h6 class="card-subtitle text-muted">Penanganan</h6></label><br>
+                            <?= $masalah_air['penanganan']; ?>
+                        </div>                    
                     <a href="<?= base_url(); ?>masalah" class="btn btn-primary">Kembali</a>
                 </div>
             </div>
